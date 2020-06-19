205a6b2c7fce6dfbc905ca4bab1aa053d15b3bfd
zxysilent <zxysilent@outlook.com>
2020-06-19 13:09:01 +0800

    [A] fix end

---
+++ src/components/layout/navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="navbar">
+		<slot></slot>
+	</div>
+</template>
+<script>
+export default {
+	name: "Navbar",
+	components: {},
+	data() {
+		return {};
+	},
+	computed: {
+		sidebar() {}
+	}
+};
+</script>
